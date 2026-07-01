8effd8fc8cebb1c7e8ec596a481bc7948112b757
jithu <jithuldevaraj01@gmail.com>
2026-07-01 15:43:45 +0530

    Done :- Generate Password

--- main.py
+++ main.py
@@ -1,10 +1,37 @@
 import tkinter
 from tkinter import messagebox
+import random
 
-from sympy.codegen.ast import none
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
+
+    password_list = []
+
+    pass_letters = [random.choice(letters) for _ in range(nr_letters)]
+    pass_symbols = [random.choice(symbols) for _ in range(nr_symbols)]
+    pass_numbers = [random.choice(numbers) for _ in range(nr_numbers)]
+
+    password_list = pass_letters + pass_symbols + pass_numbers
+
+    random.shuffle(password_list)
+
+    password = ""
+    for char in password_list:
+      password += char
+
+    print(f"Your password is: {password}")
+    password_entry.delete(0, tkinter.END)
+    password_entry.insert(0, password)
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save(event=None):
@@ -64,7 +91,7 @@ password_label.grid(row=3, column=0, sticky="E")
 password_entry = tkinter.Entry(width=21)
 password_entry.grid(row=3, column=1, sticky="EW")
 
-generate_password_button = tkinter.Button(text="Generate Password")
+generate_password_button = tkinter.Button(text="Generate Password", command=generate_password)
 generate_password_button.grid(row=3, column=2, sticky="EW") # Stretches to match the layout
 
 add_button = tkinter.Button(text="Add", width=36, command=save)
